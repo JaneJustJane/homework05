77b57d443456fd548038f78361f844a0430f9739
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-28 19:00:15 +0300

    Amn=m+n matrix

---
+++ task01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray (int [,] m1)
+{
+    for (int i=0; i<m1.GetLength(0); i++)
+    {
+        for (int j=0; j<m1.GetLength(1); j++)
+        {
+            Console.Write($"{m1[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int [,] matrix = new int [m,n];
+for (int i=0; i<matrix.GetLength(0); i++)
+{
+    for (int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j]=i+j;
+    }
+
+}
+PrintArray (matrix);
